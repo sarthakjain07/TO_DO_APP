030c1c60a1c7e3b4608df1848104414d27aaefc4
Sarthak Jain <sarthakjain7@outlook.com>
2021-04-26 14:41:37 +0530

    Worked on to add the items in the list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, {useState} from 'react'; 
 import './App.css';
 
 function App() {
+  // This function app is returning HTML but it is a special JSX(js+html) html i.e. we can write js in this html
+  const [todos, setTodos]=useState(['Do Workout','Do Meditation']);
+  const [input, setInput]=useState('');
+  console.log(input);
+
+  const addTodo = (event) => {
+    // this will work when we press the button add to do
+    console.log("Working")
+    setTodos([...todos, input]);//...todos is a spread to append in the array
+  }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>//todos is array and todo is item in array
+        ))}
+      </ul>
+
     </div>
   );
 }
